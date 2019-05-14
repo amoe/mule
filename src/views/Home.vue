55c454a024962f4cf3a22220ead68dffcfdb941c
David Banks <amoebae@gmail.com>
2019-05-14 16:14:41 +0100

    using successfully

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,18 +8,56 @@
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import PouchDB from 'pouchdb';
+import Promise from 'bluebird';
 
 interface Document {
+    meaningOfLife: number
 };
+
+function makeSleep() {
+    return new Promise(resolve => setTimeout(resolve, 5000));
+}
+
+const DOCUMENT_NAME = 'mydoc2'
+const sampleDocument = {
+    "_id": DOCUMENT_NAME,
+    "meaningOfLife": 42
+};
+
 
 export default Vue.extend({
     name: 'home',
     mounted() {
         this.$nextTick(() => {
-            console.log("foo");
-            const db = new PouchDB<Document>("http://localhost:5984");
-            console.log(db);
-            db.info();
+            console.log("inside mounted callback");
+
+            const db = new PouchDB<Document>(
+                "http://localhost:5984/alice",
+                {
+                    'auth': {
+                        'username': 'admin',
+                        'password': 'admin'
+                    }
+                }
+            );
+
+
+            // db.get(DOCUMENT_NAME).then(response => {
+            //     console.log("response is %o", response);
+            // }).catch(error => {
+            //     console.log("error is %o", error);
+            // });
+
+            db.put(sampleDocument).then(response => {
+                console.log("put worked, %o", response);
+            }).catch(error => {
+                console.log("put failed, %o", error);
+            });
+
+
+            // db.info().then(response => {
+            //     console.log("result is %o", response);
+            // });
         });
     },
     methods: {
