ce317f63ee55e3509e26a93e5ff60ea900618462
David Banks <amoebae@gmail.com>
2019-05-14 15:27:56 +0100

    remove hw component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
@@ -10,9 +9,11 @@ import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+    name: 'home',
+    mounted() {
+        this.$nextTick(() => {
+            console.log("foo");
+        });
+    }
 });
 </script>
