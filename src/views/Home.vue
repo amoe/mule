bbcdc26c705092f83a05acffc99eb9f21b50191e
David Banks <amoebae@gmail.com>
2019-05-14 15:53:39 +0100

    import code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,6 +21,37 @@ export default Vue.extend({
             console.log(db);
             db.info();
         });
+    },
+    methods: {
+        // initialCreate(db) {
+        //     db.put(sampleDocument).then(response => {
+        //         console.log("put worked, %o", response);
+        //         this.updateExisting(db);
+        //     }).catch(error => {
+        //         console.log("put failed, %o", error);
+        //     });
+        // },
+        // updateExisting(db) {
+        //     db.get(DOCUMENT_NAME).then(response => {
+        //         return db.put({
+        //             "_id": DOCUMENT_NAME,
+        //             "_rev": response._rev,
+        //             "meaningOfLife": 43
+        //         });
+        //     }).then(response => {
+        //         console.log("update succeeded, %o", response);
+        //         this.postUpdateFetch(db);
+        //     }).catch(error => {
+        //         console.log("get failed, %o", error);
+        //     });
+        // },
+        // postUpdateFetch(db) {
+        //     db.get(DOCUMENT_NAME).then(response => {
+        //         console.log("post-update fetch succeeded, %o", response);
+        //     }).catch(error => {
+        //         console.log("post update fetch failed, %o", error);
+        //     });
+        // },
     }
 });
 </script>
