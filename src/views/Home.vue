1963414adaaa5a2fbfec499f362a71395ece03c9
David Banks <amoebae@gmail.com>
2019-05-14 15:51:44 +0100

    initial pouchdb usage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,19 @@
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import PouchDB from 'pouchdb';
+
+interface Document {
+};
 
 export default Vue.extend({
     name: 'home',
     mounted() {
         this.$nextTick(() => {
             console.log("foo");
+            const db = new PouchDB<Document>("http://localhost:5984");
+            console.log(db);
+            db.info();
         });
     }
 });
