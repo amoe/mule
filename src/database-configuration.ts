fe4c4457bceae8d9037b9acf93e2f569af4e9ba2
David Banks <amoebae@gmail.com>
2019-05-15 16:27:29 +0100

    using password from env

--- src/database-configuration.ts
+++ src/database-configuration.ts
@@ -1,3 +1,3 @@
 export const DATABASE: string = "http://visarend.solasistim.net:5984/mule";
 export const USERNAME: string = "mule";
-export const PASSWORD: string = "2YSGQ8ctPgmtBW5c";
+export const PASSWORD: string = process.env.DATABASE_PASSWORD;
