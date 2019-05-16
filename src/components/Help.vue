fd2ec07490c8846cd2f2c371cfd8fc612f3a7278
David Banks <amoebae@gmail.com>
2019-05-16 13:10:19 +0100

    help

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="help">
-  <h1>Create a new taxonomy</h1>
+  <h2>Create a new taxonomy</h1>
 
   <p>
     The document selector can be used to create a new taxonomy.  When you start
@@ -8,34 +8,40 @@
     text to begin a new document with a blank taxonomy.
   </p>
 
-  <h1>Save a taxonomy</h1>
+  <h2>Save a taxonomy</h1>
   <p>
     Click Save.  This will update the selected taxonomy, as stored on the server.
   </p>
 
-  <h1>Delete a node</h1>
+  <h2>Delete a node</h1>
   <p>
     Hover over the down-arrow icon next to a node and click 'Delete'.  Note that
     this will also delete all of the node's children.
   </p>
 
-  <h1>Add a child node</h1>
+  <h2>Add a child node</h1>
 
   <p>
     Hover over the down-arrow icon next to a node and a menu will appear.  Click
     'Add node'.  This will prompt you for the name of the node.
   </p>
 
-  <h1>Set node annotation</h1>
+  <h2>Set node annotation</h1>
   <p>
     The node displays its annotation to its right.  Click on the annotation
     to set its value.
   </p>
 
-  <h1>Unlink node</h1>
+  <h2>Unlink node</h1>
   <p>
     Again, use the menu, and click 'Unlink'.  This will remove its annotation.
   </p>
+
+  <h2>Caveats</h2>
+
+  <p>Node names should be unique at the moment.</p>
+  <p>Annotation names are stored as plain strings.  There's no linkage between them.</p>
+  <p>There's no way to change the parent node of a node or subtree (i.e. move it).</p>
 </div>
 </template>
 
